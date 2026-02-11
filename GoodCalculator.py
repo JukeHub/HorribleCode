6098d97d5ee285a9045d8bf1eca49cf741593df7
Alex Roy <alexroy.sebastian@gmail.com>
2026-02-11 17:46:16 -0500

    intial good version setup

---
+++ GoodCalculator.py
@@ -0,0 +1,16 @@
+# A simple Calculator program - Good Version following KISS, DRY, and YAGNI principles
+
+# functions for each operation
+def add(a,b):
+    pass
+def subtract(a,b):
+    pass
+def multiply(a,b):
+    pass
+def divide(a,b):
+    pass
+
+# main function to handle user input
+def main():
+    pass
+main()
