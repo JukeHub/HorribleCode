809dfc50c6362868d222f381364793ddd60b0cd4
Julian Dominguez <jdominguez9@gmail.com>
2026-02-12 12:12:43 -0500

    feat: implement needless method that goes against YAGNI

--- HorribleCalculator.py
+++ HorribleCalculator.py
@@ -14,6 +14,21 @@ def multiply(number1, number2):
 def divide(number1, number2):
     pass
 
+# absolutely unnecessary method. User can just enter ints or we can typecast strings on request.
+# This makes YAGNI MAD.
+def string_to_int(word):
+    char_map = {
+        '0':0, '1':1, '2':2, '3':3, '4':4, '5':5,
+        '6':6, '7':7, '8':8, '9':9
+    }
+    number = 0
+
+    for char in word:
+        string_digit = char_map[char]
+        number = (number * 10) + string_digit
+    return number
+
+
 number1 = 3     # example of YAGNI being ignored.
 number2 = 6     # making more variable than needed, or not putting into a list
 number3 = 8     # or not asking the user for numbers
